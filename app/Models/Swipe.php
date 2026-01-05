72eecd8b75a2c5abe7e51d78243d84a3bcec33ac
joyagergess <gergessjoya@gmail.com>
2026-01-05 17:15:49 +0200

    Added Swipe and Match models with relationships

--- app/Models/Swipe.php
+++ app/Models/Swipe.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Swipe extends Model
 {
-    //
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'swiper_profile_id',
+        'swiped_profile_id',
+        'direction',
+        'created_at',
+    ];
+
+    protected $casts = [
+        'direction' => 'string',
+        'created_at' => 'datetime',
+    ];
+
+    public function swiper()
+    {
+        return $this->belongsTo(
+            Profile::class,
+            'swiper_profile_id'
+        );
+    }
+
+    public function swiped()
+    {
+        return $this->belongsTo(
+            Profile::class,
+            'swiped_profile_id'
+        );
+    }
 }
