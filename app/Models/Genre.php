e2a9ba74d49657b7d434cd54d04b4d09f1bd2957
joyagergess <gergessjoya@gmail.com>
2026-01-05 17:12:23 +0200

    Added Instrument, Genre, and Objective models

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function profiles()
+    {
+        return $this->belongsToMany(
+            Profile::class,
+            'profile_genres'
+        );
+    }
 }
