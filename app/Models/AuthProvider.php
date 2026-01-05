0191d732b4d4d39b63742491f528ffb04a4154cd
joyagergess <gergessjoya@gmail.com>
2026-01-05 17:09:19 +0200

    Added User and AuthProvider models with relationships

--- app/Models/AuthProvider.php
+++ app/Models/AuthProvider.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AuthProvider extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'provider',
+        'provider_user_id',
+    ];
+
+    protected $casts = [
+        'provider' => 'string',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
