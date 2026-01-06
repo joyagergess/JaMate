130a0d84b1273220957e7129970e8ec1942f2700
joyagergess <gergessjoya@gmail.com>
2026-01-06 18:08:56 +0200

    Fixed verification email

--- Server/app/Http/Controllers/AuthController.php
+++ Server/app/Http/Controllers/AuthController.php
@@ -55,4 +55,5 @@ class AuthController extends Controller
             'Logged out'
         );
     }
+  
 }
--- Server/routes/api.php
+++ Server/routes/api.php
@@ -4,19 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EmailVerificationController;
 
-Route::prefix('v0.1/auth')->group(function () {
+Route::prefix('v0.1')->group(function () {
 
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
+    Route::prefix('auth')->group(function () {
 
-    Route::get(
-        'email/verify/{id}/{hash}',
-        [EmailVerificationController::class, 'verify']
-    )->middleware('signed')->name('verification.verify');
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
 
-    Route::middleware('auth:api')->group(function () {
-        Route::post('email/resend', [EmailVerificationController::class, 'resend']);
-        Route::post('logout', [AuthController::class, 'logout']);
-        Route::get('me', fn () => auth()->user());
+        Route::get('email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->middleware('signed')->name('verification.verify');
+
+        Route::middleware('auth:api')->group(function () {
+            Route::post('email/resend', [EmailVerificationController::class, 'resend']);
+            Route::post('logout', [AuthController::class, 'logout']);
+        });
     });
 });
